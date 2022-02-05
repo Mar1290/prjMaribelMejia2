87f945269eb48c2aee0af3d790a549ab2d214059
POSGRADO <POSGRADO@DESKTOP-LN2IU17>
2022-02-05 15:51:35 -0600

    Agregar archivos de proyecto.

---
+++ prjMaribelMejia/Controllers/Prueba2Controller.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjMaribelMejia.Models;
+using System.Collections.Generic;
+
+namespace prjMaribelMejia.Controllers
+{
+    public class Prueba2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            //simular los datos como que estoy usando base de datos
+
+            List<Pacientes> pacientes = new List<Pacientes>();
+            pacientes.Add(new Pacientes()
+            {
+                Nombre = "Jose Mendez",
+                Direccion="Managua",
+                Edad=2,
+                Telefono="8885554"
+
+            });
+            pacientes.Add(new Pacientes()
+            {
+                Nombre = "Jose Gonzalez",
+                Direccion = "Managua",
+                Edad = 32,
+                Telefono = "451154785"
+
+            });
+
+            return View(pacientes);
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+
+        }
+}
+}
---
+++ prjMaribelMejia/Models/Pacientes.cs
@@ -0,0 +1,11 @@
+﻿namespace prjMaribelMejia.Models
+{
+    public class Pacientes
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public int Edad { get; set; }
+
+    }
+}
