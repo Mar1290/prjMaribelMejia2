f087cc7df60ba55550517d48d6698e2293311938
Maribel120290 <maribel.mejiamendoza@yahoo.es>
2022-02-05 16:17:26 -0600

    Nuevo

---
+++ prjMaribelMejia/Models/Productos.cs
@@ -0,0 +1,12 @@
+﻿namespace prjMaribelMejia.Models
+{
+    public class Productos
+    {
+        
+        public int IdProducto { get; set; }
+        public string Producto { get; set; }
+        public string Descripcion { get; set; }
+        public int IdCategoria { get; set; }
+        public string Categoria { get; set; }
+    }
+}
