954152ef6e1070a62c7475dcd51c5067302c708b
Maribell <maribel.mejiamendoza@yahoo.es>
2022-02-19 15:28:26 -0600

    editar

--- prjMaribelMejia/Models/Categorias.cs
+++ prjMaribelMejia/Models/Categorias.cs
@@ -8,14 +8,13 @@ namespace prjMaribelMejia.Models
         [Required]  
         [Key]
         public int IdCategoria { get; set; }
-        public int IdClasificacion { get; set; }
+    
         [StringLength(50)]
         [Required(ErrorMessage = "El campo categoria es requerido")]
         public string Categoria { get; set; }
         public string DescripcionCategoria { get; set; }
 
-        //PARA ENVIAR LA FECHA DEL SERVIDOR
-    
+        //PARA ENVIAR LA FECHA DEL SERVIDOR    
         public  DateTime FechaCreacionCategoria { get; set; }
  
     }
