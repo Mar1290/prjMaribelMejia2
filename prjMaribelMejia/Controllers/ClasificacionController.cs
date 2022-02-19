0d8cc6cf8c1081d33d65544816d6ec366ec254e4
MARIBELL <maribel.mejiamendoza@yahoo.es>
2022-02-18 22:50:48 -0600

    a

---
+++ prjMaribelMejia/Controllers/ClasificacionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using prjMaribelMejia.Models;
+using System.Diagnostics;
+using prjMaribelMejia.Data;
+using System.Linq;
+
+namespace prjMaribelMejia.Controllers
+{
+    public class ClasificacionController : Controller
+    {
+        //1 crear esto
+        //private readonly ILogger<HomeController> _logger;
+
+        private readonly MyDbContext _context;//MyDbCONTEXT:es ña clase que orquesta conexxion a la bd
+
+        //2. creampos el constructor de la clase
+        public ClasificacionController(ILogger<CategoriasController> logger, MyDbContext context)
+        {
+            // _logger= logger;    
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ prjMaribelMejia/Migrations/20220219013135_migracion18222.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace prjMaribelMejia.Migrations
+{
+    public partial class migracion18222 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
