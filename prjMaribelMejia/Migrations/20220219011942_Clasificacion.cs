fcc47a1067583d7324e8779b849e1855552e367e
MARIBELL <maribel.mejiamendoza@yahoo.es>
2022-02-18 19:22:34 -0600

    AVANCE

---
+++ prjMaribelMejia/Migrations/20220219011942_Clasificacion.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace prjMaribelMejia.Migrations
+{
+    public partial class Clasificacion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
