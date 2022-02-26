e3b15787898b05dec92c767a51b816f38d5cc577
Maribell <maribel.mejiamendoza@yahoo.es>
2022-02-26 14:41:50 -0600

    jscategoria

---
+++ prjMaribelMejia/Migrations/20220226200110_sesionclase5.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace prjMaribelMejia.Migrations
+{
+    public partial class sesionclase5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ prjMaribelMejia/wwwroot/js/jsCategoria.js
@@ -0,0 +1,7 @@
+﻿/*muestra alerta*/
+alert("Archivo cargado");
+/*cambia el color a al h1*/
+$('h1').css("color", "green");
+/*para cargar valores en los controles de una vista*/
+$('.nombre-categoria').val('Digite Descripción de la Categoría');
+$(".pruebajq4").val("Digite Categoría");
